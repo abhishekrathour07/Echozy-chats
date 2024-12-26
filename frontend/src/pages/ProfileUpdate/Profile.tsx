48d42b3ceee68ca0deb7c7722bd69e86b716c04e
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-12-26 23:18:57 +0530

    profile files

--- frontend/src/pages/ProfileUpdate/Profile.tsx
+++ frontend/src/pages/ProfileUpdate/Profile.tsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Profile:React.FC = () => {
     return (
-        <div>
+        <div className='bg-black h-[100vh]'>
+          <ul className='text-white bg-purple-600 h-16 flex items-center '>
+            <div className='hidden sm:flex gap-4  justify-between px-6 text-xl'>
+            <li>Name </li> 
+            <li>ABout</li>
+            <li>contact</li>
+            <li>new</li>
+            <li>Name </li>
+            <li>ABout</li>
+            <li>contact</li>
 
+            </div>
+            <p className='flex sm:hidden justify-end px-2'>three dot</p>
+          </ul>
         </div>
     )
 }
